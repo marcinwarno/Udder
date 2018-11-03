4b528afd8471435e51fc6cea17fa1c0f2ee6932d
Marcin Warno <marcin.warno@gmail.com>
2018-11-03 14:32:06 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Marcin Warno on 03/11/2018.
+//  Copyright © 2018 Marcin Warno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
